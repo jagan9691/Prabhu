46b004a425733f095e412b55788f1bfadaacc18b
jagan9691 <jagan9691@gmail.com>
2022-07-18 18:58:22 +0530

    new push

---
+++ java/org/new1/Read.java
@@ -0,0 +1,37 @@
+package org.new1;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class Read {
+	public static void main(String[] args) throws IOException {
+		File f = new File("F:\\eclipse\\Excel\\Data\\Day1.xlsx");
+
+		FileInputStream s = new FileInputStream(f);
+		Workbook w = new XSSFWorkbook(s);
+		Sheet sh = w.getSheet("Sheet1");
+		int rows = sh.getPhysicalNumberOfRows();
+		//Row r = sh.getRow(5);
+		//int cells = r.getPhysicalNumberOfCells();
+		for (int i = 0; i < sh.getPhysicalNumberOfRows(); i++) {
+
+			Row rh = sh.getRow(i);
+			for (int j = 0; j < rh.getPhysicalNumberOfCells(); j++) {
+				Cell ce = rh.getCell(j);
+				System.out.println(ce );
+
+			}
+			
+		}
+
+	}
+
+}
